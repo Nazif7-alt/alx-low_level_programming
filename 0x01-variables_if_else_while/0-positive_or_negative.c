159aea7e2c8bda510b29ee66670ef6d4a1ae276b
Nazif7-alt <arounanazif52@gmail.com>
2023-09-11 12:44:14 +0200

    all_optional_task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,15 +7,10 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
 	int n;
 	srand(time(0));
-
-
 	n = rand() - RAND_MAX / 2;
-
-	/* your code goes there */
 
 	if (n > 0)
 	{
@@ -31,4 +26,3 @@ int main(void)
 	}
 	return (0);
 }
-
