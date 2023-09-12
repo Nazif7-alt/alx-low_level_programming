97fe189c100a2961d8d0dddd57fe6ac6a4ccdd80
Nazif7-alt <arounanazif52@gmail.com>
2023-09-12 23:32:03 +0200

    first_task_today

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -point
+ *
+ * return 0 (success)
+ */
+int main(void)
+{
+	char chaine[] = "_putchar"
+		for (int i = 0; chaine[i] != '\0'; i++)
+		{
+			putchar(chaine[i]);
+		}
+		putchar('\n');
+	return (0);
+}
