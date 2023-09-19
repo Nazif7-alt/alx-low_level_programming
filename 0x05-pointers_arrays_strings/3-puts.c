9edb48677bcdbc71a86bcd400034cb4c0a2b583c
Nazif7-alt <arounanazif52@gmail.com>
2023-09-19 14:08:25 +0200

    Task3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,13 +3,12 @@
 /**
  * _puts - prints a string
  * @str: The string to print
- *
  * Return: void
  */
 
 void _puts(char *str)
 {
-	for (; *str != '\n'; str++)
+	for (; *str != '\0'; str++)
 	{
 	_putchar(*str);
 	}
