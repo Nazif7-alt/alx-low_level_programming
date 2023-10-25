e3656950b63dcdb7588ce696cf4b26d3208b6a60
Nazif7-alt <arounanazif52@gmail.com>
2023-10-25 15:03:41 +0200

    works

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - Affiche les éléments d'une liste chaînée de type listint_t.
+ * @h: Pointeur vers le premier élément de la liste.
+ *
+ * Return: Le nombre d'éléments dans la liste.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *current = h;
+
+	while (current != NULL)
+	{
+	printf("%d\n", current->n);
+	current = current->next;
+		count++;
+	}
+
+	return (count);
+}
